1c49cc07afce4e9b4272952ca3b8fcd0b4d55d1c
Eder Roger Souza <eder.roger.souza@gmail.com>
2016-07-27 19:58:02 -0300

    add example of index running with webpack , babel

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,15 +1,25 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, '/');
-var APP_DIR = path.resolve(__dirname, '/');
+var BUILD_DIR = path.resolve(__dirname, '');
+var APP_DIR = path.resolve(__dirname, '');
 
 var config = {
   entry: APP_DIR + '/components/index.js.jsx',
   output: {
-      path: BUILD_DIR,
-      filename: 'bundle.js'
-    }
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  }
+
 };
 
 module.exports = config;
